d42ce96d241bcda0929a4c64c6915a07ccc4e3da
madebyfabian <hello@madebyfabian.com>
2024-12-25 09:10:26 +0100

    step: install nuxt ui v3

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<UApp>
+		<NuxtRouteAnnouncer />
+		<NuxtPage />
+	</UApp>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="container py-12">
+		<h1>Hello world</h1>
+		<p>Welcome to your new Nuxt 3 app.</p>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
